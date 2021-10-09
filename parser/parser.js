eb4bc6d8942b61e1a88f2790013483de312a8d44
Artjoms <artv20022@gmail.com>
2021-10-09 12:50:31 +0300

    Code improved.

--- parser/parser.js
+++ parser/parser.js
@@ -5,16 +5,15 @@ const tokens = [ { TYPE: 'KEY_WORD_CLASS', position: 0, lexem: 'class' },
 { TYPE: 'ID', position: 6, lexem: 'main' },
 { TYPE: 'OPEN_BRACE', position: 11, lexem: '{' },
 { TYPE: 'KEY_WORD_VAR', position: 17, lexem: 'var' },
-{ TYPE: 'ID', position: 21, lexem: 'j' },
-{ TYPE: 'OPERATOR_ASSIGN', position: 23, lexem: '=' },
-{ TYPE: 'INT', position: 25, lexem: '30' },
-{ TYPE: 'SEMI_COLON', position: 27, lexem: ';' },
-{ TYPE: 'KEY_WORD_RETURN', position: 33, lexem: 'return' },
-{ TYPE: 'ID', position: 40, lexem: 'j' },
-{ TYPE: 'SEMI_COLON', position: 41, lexem: ';' },
-{ TYPE: 'CLOSE_BRACE', position: 43, lexem: '}' } ]
+{ TYPE: 'ID', position: 21, lexem: 'lol' },
+{ TYPE: 'OPERATOR_ASSIGN', position: 25, lexem: '=' },
+{ TYPE: 'INT', position: 27, lexem: '50' },
+{ TYPE: 'SEMI_COLON', position: 29, lexem: ';' },
+{ TYPE: 'KEY_WORD_RETURN', position: 35, lexem: 'return' },
+{ TYPE: 'ID', position: 42, lexem: 'j' },
+{ TYPE: 'SEMI_COLON', position: 43, lexem: ';' },
+{ TYPE: 'CLOSE_BRACE', position: 45, lexem: '}' } ]
 
-const syntaxTree = [];
 let next = -1;
 let state = false;
 let currentTerminals = []
@@ -41,37 +40,44 @@ const program = () => {
         return result;
     }
 
+    const node = {
+        type: "PROGRAM",
+        state: [
 
-    if (saveNext(CLASS_DECLARATION)) {
-        syntaxTree.push("CLASS_DECLARATION")
-        return true
+        ]
     }
 
+
+    if (saveNext(() => CLASS_DECLARATION(node.state))) {
+        currentTerminals.push(node)
+        return true
+    }
+    
     return false
 }
 
 
-const CLASS_DECLARATION = () => {
+const CLASS_DECLARATION = (parentNode) => {
     const node = {
         type: "CLASS_DECLARATION",
         state: [
 
         ]
     }
-    const res = term("KEY_WORD_CLASS", node.state) && term("ID", node.state) && BODY()
-    res && currentTerminals.push(node)
+    const res = term("KEY_WORD_CLASS", node.state) && term("ID", node.state) && BODY(node.state)
+    res && parentNode.push(node)
     return res
 }
 
-const BODY = () => {
+const BODY = (parentNode) => {
     const node = {
         type: "BODY",
         state: [
 
         ]
     }
-    const res = term("OPEN_BRACE", node.state) && EXPRESSION(node) && term("CLOSE_BRACE", node.state);
-    res && currentTerminals.push(node)
+    const res = term("OPEN_BRACE", node.state) && EXPRESSION(node.state) && term("CLOSE_BRACE", node.state);
+    res && parentNode.push(node)
     return res
 }
 
@@ -84,41 +90,39 @@ const EXPRESSION = (node) => {
         return result;
     }
 
-    if (saveNext(VARIABLE_DECLARATION)) {
-        syntaxTree.push("VARIABLE_DECLARATION")
-        node.state.push("VARIABLE_DECLARATION")
+    if (saveNext(() => VARIABLE_DECLARATION(node))) {
+        //node.state.push({nextType: "VARIABLE_DECLARATION"})
         return true
     }
-    else if (saveNext(RETURN_STATEMENT)) {
-        syntaxTree.push("RETURN_STATEMENT")
-        node.state.push("RETURN_STATEMENT")
+    else if (saveNext(() => RETURN_STATEMENT(node))) {
+        //node.state.push({nextType: "RETURN_STATEMENT"})
         return true
-    } else if (saveNext(EPSILON)) {
-        syntaxTree.push("EPSILON")
-        node.state.push("EPSILON")
+    } else if (saveNext(() =>EPSILON(node))) {
+        //node.state.push({nextType: "EPSILON"})
         return true
     }
 
     return false
 }
 
-const VARIABLE_DECLARATION = () => {
+const VARIABLE_DECLARATION = (parentNode) => {
     const node = {
         type: "VARIABLE_DECLARATION",
         state: [
 
         ]
     }
+
     const res = term("KEY_WORD_VAR", node.state) && term("ID", node.state) && 
     term("OPERATOR_ASSIGN", node.state) && 
-    VALUE() && 
+    VALUE(node.state) && 
     term("SEMI_COLON", node.state) && 
-    EXPRESSION(node)
-    res && currentTerminals.push(node)
+    EXPRESSION(node.state)
+    res && parentNode.push(node)
     return res
 }
 
-const RETURN_STATEMENT = () => {
+const RETURN_STATEMENT = (parentNode) => {
     const node = {
         type: "RETURN_STATEMENT",
         state: [
@@ -127,11 +131,12 @@ const RETURN_STATEMENT = () => {
     }
     const res = term("KEY_WORD_RETURN", node.state) && term("ID", node.state) && 
     term("SEMI_COLON", node.state)
-    res && currentTerminals.push(node)
+    res && parentNode.push(node)
     return res
 }
 
-const EPSILON = () => {
+
+const EPSILON = (parentNode) => {
     const node = {
         type: "EPSILON",
         state: [
@@ -139,11 +144,11 @@ const EPSILON = () => {
         ]
     }
     const res = term("EPSILON", node.state)
-    res && currentTerminals.push(node)
+    res && parentNode.push(node)
     return res
 }
 
-const VALUE = () => {
+const VALUE = (parentNode) => {
     let save = next;
 
     const saveNext = (callback) => {
@@ -152,15 +157,7 @@ const VALUE = () => {
         return result;
     }
 
-    const node = {
-        type: "VALUE",
-        state: [
-
-        ]
-    }
-
-    if (saveNext(() => term("INT", node.state))) {
-        syntaxTree.push(node)
+    if (saveNext(() => term("INT", parentNode))) {
         return true
     }
 
@@ -168,7 +165,6 @@ const VALUE = () => {
 }
 
 program()
-console.log(syntaxTree)
 
 const output = JSON.stringify(currentTerminals.reverse())
 console.log(output)
